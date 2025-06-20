bcc821b465d71bf022c197f6b9b34e97f1277794
pppatil7 <pundalikpatil7218@gmail.com>
2025-06-20 07:51:11 +0530

    added repo

---
+++ src/main/java/com/ems/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.ems.repositories;
+
+import com.ems.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
